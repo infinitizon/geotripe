bf119bf1c4bc971b2e4f442a9c446b0963b34cd6
Abimbola <infinitizon@yahoo.com>
2016-07-04 08:23:24 +0100

    Login

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -34,9 +34,6 @@ angular.module('School', ['Auth', 'Home', 'ui.router', 'ngCookies'])
       function ($rootScope, $location, $cookieStore, $http) {
          // keep user logged in after page refresh
          $rootScope.globals = $cookieStore.get('globals') || {};
-         if ($rootScope.globals.currentUser) {
-            $http.defaults.headers.common['Authorization'] = 'Basic ' + $rootScope.globals.currentUser.authdata; // jshint ignore:line
-         }
          $rootScope.$on('$locationChangeStart', function (event, next, current) {
             // redirect to login page if not logged in
             if ($location.path() !== '/login' && !$rootScope.globals.currentUser) {
