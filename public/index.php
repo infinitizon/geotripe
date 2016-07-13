3c713f1d147ea70abe833e04f1eeb55ff7379399
Abimbola <infinitizon@yahoo.com>
2016-07-13 22:55:41 +0100

    Still on the Login

--- public/index.php
+++ public/index.php
@@ -7,11 +7,12 @@
         <link href="scripts/bootstrap/3.3.6/css/bootstrap.min.css" rel="stylesheet" type="text/css"/>
         <link href="https://fonts.googleapis.com/css?family=Open+Sans:300" rel="stylesheet">
         <link rel="stylesheet" href="font-awesome-4.6.3/css/font-awesome.min.css">
-        <link href="css/app.css" rel="stylesheet" type="text/css"/>
+        <link href="css/app2.css" rel="stylesheet" type="text/css"/>
         <link rel="shortcut icon" href="images/favicon.ico" type="image/x-icon">
         <link rel="icon" href="images/favicon.ico" type="image/x-icon">
     </head>
     <body>
+        <!-- https://apps.gndf.io/#/settings/project/ -->
         <div ui-view></div>
         
         <!--script src="//ajax.googleapis.com/ajax/libs/angularjs/1.5.7/angular.min.js"></script-->
