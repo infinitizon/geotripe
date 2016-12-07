8cad1490f0ce18f04ed0371d4222c5e761fdcadd
infinitizon <ahassan@chapelhilldenham.com>
2016-12-07 18:35:38 +0100

    Working on the RFQ print

--- public/modules/quote/controller.js
+++ public/modules/quote/controller.js
@@ -30,10 +30,45 @@ angular.module('RFQ', ['angularUtils.directives.dirPagination','ui.select'])
             }
             vm.getQuoteSummary();
         }])
-    .controller('PrintRFQController', ['$scope', '$location', '$localStorage','DataService','CommonServices','$http', '$uibModal',
-        function ($scope, $location, $localStorage, DataService,CommonServices,$http, $uibModal) {
+    .controller('PrintRFQController', ['$scope', '$location', '$localStorage','DataService','CommonServices','$stateParams', '$uibModal',
+        function ($scope, $location, $localStorage, DataService,CommonServices,$stateParams, $uibModal) {
             var vm = this;
             vm.currentDate = new Date();
+            var data=angular.copy(CommonServices.postData);
+            data.factName = "Quote q, Party p, State s, Country c";
+            data.transactionMetaData.queryMetaData.joinClause = {
+                'joinType':['JOIN','LEFT JOIN','LEFT JOIN'],'joinKeys':['q.Party_Party_Id=p.Party_Id','p.party_state_id=s.state_id','p.party_country_id=c.country_id']
+            }
+            data.transactionMetaData.responseDataProperties = 'q.subject&q.rfq_no&q.party_party_id&q.eventowner&p.name clientname&p.addressline1&p.addressline2&p.addresscity&s.name state&c.name country&p.emailaddress&(select SUM(quantity*unitprice) FROM QuoteDetail WHERE quote_quote_id=q.quote_id)sum';
+            data.transactionMetaData.queryMetaData.queryClause.andExpression = [
+                {
+                    "propertyName": "q.quote_id",
+                    "propertyValue": $stateParams.print,
+                    "propertyDataType": "BIGINT",
+                    "operatorType": "="
+                }
+            ];
+            DataService.post('inboundService', data).then(function (response) {
+                vm.master=response.data.data[0];
+            })
+            var data=angular.copy(CommonServices.postData);
+            data.factName = "QuoteDetail qd,UnitOfMeasure uom";
+            data.transactionMetaData.queryMetaData.queryClause.andExpression = [
+                {
+                    "propertyName": "qd.quote_quote_id",
+                    "propertyValue": $stateParams.print,
+                    "propertyDataType": "BIGINT",
+                    "operatorType": "="
+                }
+            ];
+            data.transactionMetaData.queryMetaData.joinClause = {
+                'joinType':['JOIN'],'joinKeys':['qd.unitofmeasure=uom.unitofmeasure_id']
+            }
+            data.transactionMetaData.responseDataProperties = 'qd.partno_modelno&(CASE WHEN qd.oem_description IS NOT NULL THEN qd.oem_description ELSE qd.description END)description&qd.quantity&uom.name unitofmeasure&qd.unitprice';
+            DataService.post('inboundService', data).then(function (response) {
+                vm.detail=response.data.data
+            })
+
         }])
     .controller('QuoteByStatusController', ['$http', '$scope', '$location', '$localStorage','DataService','CommonServices','$stateParams', '$uibModal', 'ImportExportToExcel',
         function ($http, $scope, $location, $localStorage, DataService, CommonServices, $stateParams, $uibModal,ImportExportToExcel) {
