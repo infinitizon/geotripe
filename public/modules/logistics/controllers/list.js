c315fdb675bd40385156181f71593233f88708ba
Abimbola <infinitizon@yahoo.com>
2017-02-13 15:20:00 +0100

    Completed the approval sequence for POs

--- public/modules/logistics/controllers/list.js
+++ public/modules/logistics/controllers/list.js
@@ -20,18 +20,17 @@ angular.module('Logistics')
             vm.getData = function(pageno, filters) {
                 vm.quotes = []; // Initially make list empty so as to show the "loading data" notice!
                 var data=angular.copy(CommonServices.postData);
-                data.factName = 'Quote q, Party p, QuoteStatus qs, Users u, QuoteDetail qd';
+                data.factName = 'Quote q, QuoteCat qc, Party p, QuoteStatus qs, Users u, QuoteDetail qd';
                 /** We could exclude weekends in datediff using the following
                  * SELECT (5 * (DATEDIFF('2016-08-31', '2016-08-01') DIV 7) + MID('0123444401233334012222340111123400012345001234550', 7 * WEEKDAY('2016-08-01') + WEEKDAY('2016-08-31') + 2, 1))
                  */
-                data.transactionMetaData.responseDataProperties = 'q.quote_id&q.rfq_no&p.name&CONCAT(LEFT(q.subject , 30),IF(LENGTH(q.subject)>30, "…", "")) subject&CONCAT(u.lastname,", ",u.middlename," ",u.firstname)enteredBy&qs.name status&COUNT(qd.Quote_quote_Id) totalQuotes&DATEDIFF(q.duedate,NOW())remDays&q.entrydate&q.po_is_approved&q.notes';
+                data.transactionMetaData.responseDataProperties = 'q.quote_id&q.rfq_no&p.name&CONCAT(LEFT(q.subject , 30),IF(LENGTH(q.subject)>30, "…", "")) subject&CONCAT(u.lastname,", ",u.middlename," ",u.firstname)enteredBy&qs.name status&COUNT(qd.Quote_quote_Id) totalQuotes&DATEDIFF(q.duedate,NOW())remDays&q.entrydate&q.po_is_approved&q.notes&qc.po_is_approved';
                 data.transactionMetaData.pageno = pageno-1;
                 data.transactionMetaData.itemsPerPage = vm.itemsPerPage;
                 data.transactionMetaData.queryMetaData.queryClause.andExpression = [{
-                    "propertyName": "q.po_no",
-                    "propertyValue": 'NULL',
-                    "propertyDataType": "VARCHAR",
-                    "operatorType": "IS"
+                    "propertyName": "q.po_no", "propertyValue": 'NULL', "propertyDataType": "VARCHAR", "operatorType": "IS"
+                },{
+                    "propertyName": "qc.po_is_approved", "propertyValue": 'NULL', "propertyDataType": "VARCHAR", "operatorType": "IS"
                 }];
                 if(angular.isDefined(filters)){
                     if(angular.isDefined(filters.andExpre)){
@@ -41,7 +40,7 @@ angular.module('Logistics')
                     }
                 }
                 data.transactionMetaData.queryMetaData.joinClause = {
-                    'joinType':['JOIN','JOIN','JOIN','LEFT JOIN'],'joinKeys':['q.Party_Party_Id=p.Party_Id','q.Quote_Status_Id=qs.QuoteStatus_Id','q.quote_enteredBy_id=u.user_id','q.quote_Id=qd.Quote_quote_Id']
+                    'joinType':['LEFT JOIN','JOIN','JOIN','JOIN','LEFT JOIN'],'joinKeys':['q.quote_id=qc.quote_id','q.Party_Party_Id=p.Party_Id','q.Quote_Status_Id=qs.QuoteStatus_Id','q.quote_enteredBy_id=u.user_id','q.quote_Id=qd.Quote_quote_Id']
                 };
                 data.transactionMetaData.groupingProperties.by = 'q.quote_Id';
                 DataService.post('inboundService', data).then(function (response) {
@@ -60,10 +59,7 @@ angular.module('Logistics')
                 vm.filterOpts.andExpre = [];
                 if(angular.isDefined($scope.filters.rfqno)){
                     vm.filterOpts.andExpre.push({
-                        "propertyName": "q.rfq_no",
-                        "propertyValue": $scope.filters.rfqno,
-                        "propertyDataType": "VARCHAR",
-                        "operatorType": "LIKE"
+                        "propertyName": "q.rfq_no", "propertyValue": $scope.filters.rfqno, "propertyDataType": "VARCHAR", "operatorType": "LIKE"
                     })
                 }
                 vm.getData(vm.pageno, vm.filterOpts);
--- public/modules/logistics/controllers/view.js
+++ public/modules/logistics/controllers/view.js
@@ -20,24 +20,22 @@ angular.module('Logistics')
             vm.getData = function(pageno, filters) {
                 vm.quotes = []; // Initially make list empty so as to show the "loading data" notice!
                 var data=angular.copy(CommonServices.postData);
-                data.factName = 'Quote q, Party p, QuoteStatus qs, Users u, QuoteDetail qd';
+                data.factName = 'Quote q, QuoteCat qc, Party p, QuoteStatus qs, Users u, QuoteDetail qd';
                 /** We could exclude weekends in datediff using the following
                  * SELECT (5 * (DATEDIFF('2016-08-31', '2016-08-01') DIV 7) + MID('0123444401233334012222340111123400012345001234550', 7 * WEEKDAY('2016-08-01') + WEEKDAY('2016-08-31') + 2, 1))
                  */
-                data.transactionMetaData.responseDataProperties = 'q.quote_id&q.rfq_no&p.name&CONCAT(LEFT(q.subject , 30),IF(LENGTH(q.subject)>30, "…", "")) subject&CONCAT(u.lastname,", ",u.middlename," ",u.firstname)enteredBy&qs.name status&COUNT(qd.Quote_quote_Id) totalQuotes&DATEDIFF(q.duedate,NOW())remDays&q.entrydate'
+                data.transactionMetaData.responseDataProperties = 'q.quote_id&q.rfq_no&p.name&CONCAT(LEFT(q.subject , 30),IF(LENGTH(q.subject)>30, "…", "")) subject&CONCAT(u.lastname,", ",u.middlename," ",u.firstname)enteredBy&qs.name status&COUNT(qd.Quote_quote_Id) totalQuotes&DATEDIFF(q.duedate,NOW())remDays&q.entrydate&qc.po_is_approved'
                 data.transactionMetaData.pageno = pageno-1;
                 data.transactionMetaData.itemsPerPage = vm.itemsPerPage;
-                data.transactionMetaData.queryMetaData.queryClause.andExpression = [{
-                        "propertyName": "q.po_no",
-                        "propertyValue": 'NULL',
-                        "propertyDataType": "VARCHAR",
-                        "operatorType": "IS NOT"
-                    },
+                data.transactionMetaData.queryMetaData.queryClause.andExpression = [
+                    //{
+                    //    "propertyName": "q.po_no", "propertyValue": 'NULL', "propertyDataType": "VARCHAR", "operatorType": "IS NOT"
+                    //}
+                    //,{
+                    //    "propertyName": "qd.quote_is_po", "propertyValue": '1', "propertyDataType": "VARCHAR", "operatorType": "="
+                    //}
                     {
-                        "propertyName": "qd.quote_is_po",
-                        "propertyValue": '1',
-                        "propertyDataType": "VARCHAR",
-                        "operatorType": "="
+                        "propertyName": "qc.po_is_approved", "propertyValue": 'NULL', "propertyDataType": "VARCHAR", "operatorType": "IS NOT"
                     }
                 ]
                 if(angular.isDefined(filters)){
@@ -48,7 +46,7 @@ angular.module('Logistics')
                     }
                 }
                 data.transactionMetaData.queryMetaData.joinClause = {
-                    'joinType':['JOIN','JOIN','JOIN','LEFT JOIN'],'joinKeys':['q.Party_Party_Id=p.Party_Id','q.Quote_Status_Id=qs.QuoteStatus_Id','q.quote_enteredBy_id=u.user_id','q.quote_Id=qd.Quote_quote_Id']
+                    'joinType':['LEFT JOIN','JOIN','JOIN','JOIN','LEFT JOIN'],'joinKeys':['q.quote_id=qc.quote_id','q.Party_Party_Id=p.Party_Id','q.Quote_Status_Id=qs.QuoteStatus_Id','q.quote_enteredBy_id=u.user_id','q.quote_Id=qd.Quote_quote_Id']
                 }
                 data.transactionMetaData.groupingProperties.by = 'q.quote_Id';
                 DataService.post('inboundService', data).then(function (response) {
