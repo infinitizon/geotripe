58fb3f802bdfc68a2eb0a5d31ea022e3f42b13ef
Abimbola Hassan <ahassan@chapelhilldenham.com>
2020-10-14 18:23:36 +0100

    Correct the error on the model

--- Untitled.sql
+++
@@ -1,7 +0,0 @@
-SELECT * FROM geotripe.logs;
-truncate geotripe.logs;
-
-INSERT INTO logs (users_user_id,log_table,log_table_key,log_changes,log_date) VALUES 
-( 20160713, 'Quote', 20160858, 'Updated Quote 20160858: po_no=PO4332,...made changes
- on the QuoteDetail-> Updated 235366798: quote_is_po<=>1po_no=PO4332,...made changes on the QuoteDetail-
->
